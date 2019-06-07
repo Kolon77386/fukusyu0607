5c33c7dccc9b7f214826eb3e7b483ebf845dca4b
ookamiyoujyo <ookamiyoujyo@gmail.com>
2019-06-07 13:36:53 +0900

    ラベルの反射

--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -12,9 +12,43 @@ namespace fukusyu0607
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Text=MousePosition.X+", "+MousePosition.Y;
+            Point p=PointToClient(MousePosition);
+
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label1.Left > ClientSize.Width - label1.Width) 
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Top > ClientSize.Height - label1.Height)  
+            {
+                vy = -Math.Abs(vy);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
